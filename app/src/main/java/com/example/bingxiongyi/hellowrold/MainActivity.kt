5b4d7a48f99aa0f2dcefee1b400f364dacf99b2c
yibingxiong <yibingxiong@126.com>
2018-11-25 13:26:41 +0800

    以编程方式写UI

--- app/src/main/java/com/example/bingxiongyi/hellowrold/MainActivity.kt
+++ app/src/main/java/com/example/bingxiongyi/hellowrold/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.bingxiongyi.hellowrold
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun clickHandler (source: View) {
-        val tv: TextView = findViewById<TextView>(R.id.show)
-        tv.text = "Hello Android-" + java.util.Date()
+        val layout = LinearLayout(this)
+        super.setContentView(layout)
+        layout.orientation = LinearLayout.VERTICAL
+        val show = TextView(this)
+        val btn = Button(this)
+        btn.setText(R.string.ok)
+        btn.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        layout.addView(show)
+        layout.addView(btn)
+        btn.setOnClickListener {
+            show.text = "Hello world, Android, " + java.util.Date()
+        }
     }
 }
