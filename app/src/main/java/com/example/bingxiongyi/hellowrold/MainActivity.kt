821c5a23f78f838e1410faf008c84e3a20b299a5
yibingxiong <yibingxiong@126.com>
2018-11-25 17:59:59 +0800

    混合kotlin xml 写UI

--- app/src/main/java/com/example/bingxiongyi/hellowrold/MainActivity.kt
+++ app/src/main/java/com/example/bingxiongyi/hellowrold/MainActivity.kt
@@ -1,29 +1,35 @@
 package com.example.bingxiongyi.hellowrold
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.os.PersistableBundle
+import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+    private var images = intArrayOf(R.drawable.java, R.drawable.javaee, R.drawable.swift, R.drawable.ajax, R.drawable.html)
+    private var currentImg = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val layout = LinearLayout(this)
-        super.setContentView(layout)
-        layout.orientation = LinearLayout.VERTICAL
-        val show = TextView(this)
-        val btn = Button(this)
-        btn.setText(R.string.ok)
-        btn.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        layout.addView(show)
-        layout.addView(btn)
-        btn.setOnClickListener {
-            show.text = "Hello world, Android, " + java.util.Date()
+        // 获取LinearLayout布局容器
+        val main = findViewById<LinearLayout>(R.id.root)
+        // 程序创建ImageView组件
+        val image = ImageView(this)
+        image.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT)
+        // 将ImageView组件添加到LinearLayout布局容器中
+        main.addView(image)
+        // 初始化时显示第一张图片
+        image.setImageResource(images[0])
+        image.setOnClickListener {
+            // 改变ImageView里显示的图片
+            image.setImageResource(images[++currentImg % images.size])
         }
     }
+
 }
