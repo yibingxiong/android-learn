37f553781043a50e1e1858e68b876be157f5e8b6
yibingxiong <yibingxiong@126.com>
2018-12-09 17:42:49 +0800

    GridLayout

--- app/src/main/java/com/example/bingxiongyi/hellowrold/MainActivity.kt
+++ app/src/main/java/com/example/bingxiongyi/hellowrold/MainActivity.kt
@@ -2,18 +2,35 @@ package com.example.bingxiongyi.hellowrold
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.GridLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 
 class MainActivity : Activity() {
 
-    private var images = intArrayOf(R.drawable.java, R.drawable.javaee, R.drawable.swift, R.drawable.ajax, R.drawable.html)
-//    private var currentImg = 0
+    private var chars = arrayOf("7", "8", "9", "➗",
+        "4", "5", "6", "x",
+        "1", "2", "3", "-",
+        ".", "0", "=", "+"
+        )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val gridLayout = findViewById<GridLayout>(R.id.root)
+        for (i in chars.indices) {
+            val btn = Button(this)
+            btn.text = chars[i]
+            btn.textSize = 40F
+            btn.setPadding(5, 35, 5, 35)
+            val rosSpec = GridLayout.spec(i / 4 + 2)
+            val columnSpec = GridLayout.spec(i % 4)
+            val params = GridLayout.LayoutParams(rosSpec, columnSpec)
+            params.setGravity(Gravity.FILL)
+            gridLayout.addView(btn, params)
+        }
     }
-
 }
